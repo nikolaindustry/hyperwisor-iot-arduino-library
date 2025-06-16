c790c442db05ad2cae63e3ee674f15ee76c8d12c
NIKOLAINDUSTRY <nikolaindustry@gmail.com>
2025-06-16 21:10:34 +0530

    Update hyperwisor-iot.h

--- src/hyperwisor-iot.h
+++ src/hyperwisor-iot.h
@@ -4,17 +4,49 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <nikolaindustry-realtime.h>
+#include <WebServer.h>
+#include <Preferences.h>
+#include <Update.h>
+#include <DNSServer.h>
+#include <HTTPClient.h>
 
-class HyperwisorIOT {
+
+class HyperwisorIOT
+{
 public:
   HyperwisorIOT();
-  void begin(const char *ssid, const char *password, const char *deviceId);
+  void begin();
   void loop();
 
 private:
+  // WiFi & Real-time Communication
   nikolaindustryrealtime realtime;
-  void connectToWiFi(const char *ssid, const char *password);
+  WebServer server;
+  DNSServer dnsServer;
+  HTTPClient http;
+
+
+  // Core functions
   void setupMessageHandler();
+  void performOTA(const char *otaUrl);
+  void getcredentials();
+  void startAPMode();
+  void handle_provision();
+  void connectToWiFi();
+  String getSuccessHtml();
+  String getErrorHtml(String errorMessage);
+
+  // Credentials and config
+  String ssid, password, userid, deviceid, productid, email, loaclip, macid, newtarget, versionid;
+  const char *apSSID = "NIKOLAINDUSTRY_Setup";
+  const char *apPassword = "0123456789";
+  String fversion = "0.0.1";
+
+  // Retry Logic
+  unsigned long lastReconnectAttempt = 0;
+  const unsigned long reconnectInterval = 10000;
+  int retryCount = 0;
+  const int maxRetries = 6;
 };
 
 #endif
