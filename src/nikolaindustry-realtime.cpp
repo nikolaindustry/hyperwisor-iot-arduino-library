e39790da26eb0c1fa188dd2d513e7d608de80294
NIKOLAINDUSTRY <nikolaindustry@gmail.com>
2025-06-17 08:49:28 +0530

    Add files via upload

---
+++ src/nikolaindustry-realtime.cpp
@@ -0,0 +1,92 @@
+#include "nikolaindustry-realtime.h"
+
+nikolaindustryrealtime::nikolaindustryrealtime() {}
+
+void nikolaindustryrealtime::begin(const char *_deviceId)
+{
+  deviceId = _deviceId;
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    connect();
+  }
+  else
+  {
+    Serial.println("❌ WiFi not connected. WebSocket not started.");
+  }
+}
+
+void nikolaindustryrealtime::connect()
+{
+  webSocket.beginSSL("nikolaindustry-realtime.onrender.com", 443, ("/?id=" + deviceId).c_str());
+
+  webSocket.onEvent([this](WStype_t type, uint8_t *payload, size_t length)
+                    {
+    DynamicJsonDocument doc(2048);
+    switch (type) {
+      case WStype_CONNECTED:
+        Serial.println("🟢 nikolaindustry-realtime connected");
+        if (onConnectionStatusChange) onConnectionStatusChange(true);
+        break;
+      case WStype_DISCONNECTED:
+        Serial.println("🔴 nikolaindustry-realtime disconnected");
+        if (onConnectionStatusChange) onConnectionStatusChange(false);
+        break;
+      case WStype_TEXT:
+        
+        if (!deserializeJson(doc, payload, length) && onMessageCallback) {
+          JsonObject obj = doc.as<JsonObject>();
+          onMessageCallback(obj);
+        }
+        break;
+      default:
+        break;
+    } });
+
+  webSocket.setReconnectInterval(5000);
+}
+
+void nikolaindustryrealtime::loop()
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    webSocket.loop();
+  }
+}
+
+void nikolaindustryrealtime::sendJson(const JsonObject &json)
+{
+  String output;
+  if (serializeJson(json, output))
+  {
+    webSocket.sendTXT(output);
+  }
+  else
+  {
+    Serial.println("❌ Failed to serialize JSON!");
+  }
+}
+
+void nikolaindustryrealtime::sendTo(const String &targetId, std::function<void(JsonObject &)> payloadBuilder)
+{
+  DynamicJsonDocument doc(512);
+  doc["targetId"] = targetId;
+  JsonObject payload = doc.createNestedObject("payload");
+  payloadBuilder(payload);
+  sendJson(doc.as<JsonObject>());
+}
+
+void nikolaindustryrealtime::setOnMessageCallback(std::function<void(JsonObject &)> callback)
+{
+  onMessageCallback = callback;
+}
+
+void nikolaindustryrealtime::setOnConnectionStatusChange(std::function<void(bool)> callback)
+{
+  onConnectionStatusChange = callback;
+}
+
+bool nikolaindustryrealtime::isNikolaindustryRealtimeConnected()
+{
+  return webSocket.isConnected();
+}
---
+++ src/nikolaindustry-realtime.h
@@ -0,0 +1,32 @@
+#ifndef NIKOLAINDUSTRY_REALTIME_H
+#define NIKOLAINDUSTRY_REALTIME_H
+
+#include <Arduino.h>
+#include <WebSocketsClient.h>
+#include <ArduinoJson.h>
+#include <functional>
+
+class nikolaindustryrealtime
+{
+public:
+  nikolaindustryrealtime();
+  void begin(const char *deviceId);
+  void loop();
+  void sendJson(const JsonObject &json);
+  void sendTo(const String &targetId, std::function<void(JsonObject &)> payloadBuilder);
+
+  void setOnMessageCallback(std::function<void(JsonObject &)> callback);
+  void setOnConnectionStatusChange(std::function<void(bool)> callback);
+  bool isNikolaindustryRealtimeConnected();
+
+private:
+  WebSocketsClient webSocket;
+  String deviceId;
+
+  std::function<void(JsonObject &)> onMessageCallback;
+  std::function<void(bool)> onConnectionStatusChange;
+
+  void connect();
+};
+
+#endif
