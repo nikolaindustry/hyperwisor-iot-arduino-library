d72511926aee0e6148cad940cda180a0c943c72f
NIKOLAINDUSTRY <nikolaindustry@gmail.com>
2025-10-21 22:36:01 +0530

    Clean up commented command handling code
    Removed commented-out code for command handling.

--- examples/01_basic_setup.ino
+++ examples/01_basic_setup.ino
@@ -2,56 +2,55 @@
 
 HyperwisorIOT hyper;
 
+String deviceId = "";
+String userId = "";
+String msgfrom = "";
+
 void setup() {
+  Serial.begin(115200);
 
   hyper.setApiKeys("mk_4a9c4f312...", "msk_b78602...");
 
   hyper.setTimezone("IST");
 
   hyper.begin();
+  deviceId = hyper.getDeviceId();
+  userId = hyper.getUserId();
+
 
   hyper.setUserCommandHandler([](JsonObject& msg) {
-  //   {
-  //   "payload": {
-  //     "commands": [
-  //       {
-  //         "command": "Operate",
-  //         "actions": [
-  //           {
-  //             "action": "Lock",
-  //             "params": { "gpio": "12", "status": "HIGH" }
-  //           },
-  //           {
-  //             "action": "Unlock",
-  //             "params": { "gpio": "12", "status": "LOW" }
-  //           }
-  //         ]
-  //       }
-  //     ]
-  //   }
-  // }
-    
     if (!msg.containsKey("payload")) return;
+    msgfrom = msg["from"].as<String>();
+    Serial.println(msgfrom);
+
     JsonObject payload = msg["payload"];
+
+    // Get "get_ui" command
+    String command = payload["command"].as<String>();
+    Serial.println(command);
+    if (command == "get_ui") {
+      //add update dashbaord logic here
+    }
+
 
     // Get "Operate" command
     JsonObject operate = hyper.findCommand(payload, "Operate");
     if (operate.isNull()) return;
     Serial.println("Operate command found");
 
-    // Lock action
-    JsonObject lockParams = hyper.findParams(payload, "Operate", "Lock");
-    if (!lockParams.isNull()) {
-      String gpio = lockParams["gpio"] | "N/A";
-      String status = lockParams["status"] | "N/A";
-    }
+    // // Lock action
+    // JsonObject lockParams = hyper.findParams(payload, "Operate", "Lock");
+    // if (!lockParams.isNull()) {
+    //   String gpio = lockParams["gpio"] | "N/A";
+    //   String status = lockParams["status"] | "N/A";
+    // }
 
-    // Unlock action
-    JsonObject unlockParams = hyper.findParams(payload, "Operate", "Unlock");
-    if (!unlockParams.isNull()) {
-      String gpio = unlockParams["gpio"] | "N/A";
-      String status = unlockParams["status"] | "N/A";
-    }
+    // // Unlock action
+    // JsonObject unlockParams = hyper.findParams(payload, "Operate", "Unlock");
+    // if (!unlockParams.isNull()) {
+    //   String gpio = unlockParams["gpio"] | "N/A";
+    //   String status = unlockParams["status"] | "N/A";
+    // }
   });
 }
 
